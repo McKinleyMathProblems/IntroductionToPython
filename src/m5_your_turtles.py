82eeb06ed450e8980de06da45d76c5c8c22ad908
Dave Fisher <fisherds@rose-hulman.edu>
2020-08-16 18:39:26 -0400

    turtles

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and McKinley.
 """
 ########################################################################
-# TODO: 1.
+# Done: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# done: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -28,3 +28,40 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+marie = rg.SimpleTurtle('turtle')
+marie.pen = rg.Pen('purple', 3)
+marie.speed = 20
+
+size = 200
+
+for k in range(30):
+    marie.draw_circle(size)
+    marie.pen_up()
+    marie.right(45)
+    marie.forward(10)
+    marie.left(45)
+
+    marie.pen_down()
+    size = size - 12
+
+bowen = rg.SimpleTurtle('turtle')
+bowen.pen = rg.Pen('dark green', 3)
+bowen.speed = 20
+size = 300
+
+for k in range(30):
+    bowen.draw_regular_polygon(11, 100)
+    bowen.pen_up()
+    bowen.right(45)
+    bowen.forward(10)
+    bowen.left(45)
+    bowen.pen_down()
+    size = size - 12
+
+window.close_on_mouse_click()
